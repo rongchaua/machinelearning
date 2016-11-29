cac2983e4df176da19b3b43850da44b96567ca17
rongchaua <4nh7i3m@gmail.com>
2016-11-29 16:54:51 +0100

    add other training models

--- regression_algorithms.py
+++ regression_algorithms.py
@@ -1,10 +1,14 @@
 """Regression Algorithm"""
 import os
-import mathplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
-
+from sklearn import linear_model
+from sklearn.linear_model import Ridge
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.externals import joblib
 
 def get_home_data():
     """Get home data, from local csv."""
@@ -16,23 +20,72 @@ def get_home_data():
 
 def plotting_features_vs_target(features, x,y):
     num_feature = len(features)
-    f,axes = pl
+    f,axes = plt.subplots(1,num_feature,sharey=True)
+
+    for i in range(0, num_feature):
+        axes[i].scatter(x[features[i]],y)
+        axes[i].set_title(features[i])
+
+    plt.show()
 
 
 def main():
     """Main function"""
     dataframe = get_home_data()
 
-    features = ["bedrooms", "bathrooms", "grade"]
+#    features = ["bedrooms", "bathrooms", "grade"]
+    features = ["bedrooms", "bathrooms"]
     print("Features name: ", dataframe.columns.values)
     print("Selected features: ", features)
 
     y = dataframe["price"]
-    X = dataframe[features]
-    x_train, x_test, y_train, y_test = train_test_split(X,y,test_size=0.3)
+    x = dataframe[features]
+    x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.3)
 
     plotting_features_vs_target(features, x_train,y_train)
 
+    """
+    DEFAULT MODEL
+    """
+    linear = linear_model.LinearRegression()
+    linear.fit(x_train, y_train)
+    print("Coef linear:", linear.coef_)
+    print("Intercept:", linear.intercept_)
+    score_trained = linear.score(x_test,y_test)
+    print("Model scored:", score_trained)
+    
+#    """
+#    LASSO MODEL
+#    """
+#    lasso_linear = linear_model.Lasso(alpha=1.0)
+#    lasso_linear.fit(x_train,y_train)
+#    
+#    score_lasso_trained = lasso_linear.score(x_test,y_test)
+#    print ("Lasso model scored:", score_lasso_trained)
+#
+#    """
+#    RIDGE MODEL
+#    """
+#    ridge_linear = Ridge(alpha=1.0)
+#    ridge_linear.fit(x_train,y_train)
+#    
+#    score_ridge_trained = ridge_linear.score(x_test,y_test)
+#    print("Ridge model scored:", score_ridge_trained)
+#    
+#    """
+#    POLYNOMIAL REGRESSION
+#    """
+#    poly_model = Pipeline([('poly', PolynomialFeatures(interaction_only=True,degree=2)),
+#                           ('linear', linear_model.LinearRegression(fit_intercept=False))])
+#    poly_model = poly_model.fit(x_train,y_train)
+#    score_poly_trained = poly_model.score(x_test,y_test)
+#    print("Poly model scored:", score_poly_trained)
+#    
+#    pkl = "models/linear_model_v1.pkl"
+#    joblib.dump(linear, pkl)    
+#    clf = joblib.load(pkl)
+#    predicted = clf.predict(x_test)
+#    print("Predicted test:", predicted)
 
 if __name__ == "__main__":
     main()
